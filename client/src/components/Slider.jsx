fc9c8f5f9206e4bc688d2f30c6621dd6c1a6ec9d
LuthoJara <Luthojaraofficial@gmail.com>
2023-02-17 10:28:57 +0200

    Mostly finished the slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,17 +1,22 @@
 import styled from "styled-components";
-import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
-import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+
+import { useState } from "react";
+
+import {sliderItems}  from "../data";
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: #ffefef;
+    background-color: #f8f1f9;
     border-radius: 50%;
     display: flex;
     align-items: center;
@@ -24,41 +29,80 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transform: translateX(${props => props.slideIndex * -100}vw);
 `
 const Slide = styled.div`
     display: flex;
     align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${props=> props.bg};
 `
 const ImgContainer = styled.div`
     flex: 1;
+    height: 100%;
+    padding: 50px;
 `
 const Image = styled.img`
-    
+    height: 80%;
 `
 const InfoContainer = styled.div`
     flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    font-size: 60px;
+`
+const Description = styled.p`
+    margin: 40px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 2px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 
 const Slider = () => {
-  return (
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+        if (direction === "left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex - 0 : 2);
+        } else {
+            setSlideIndex(slideIndex < 0 ? slideIndex + 1 : 0);
+        }
+    };
+
+    return (
     <Container>
-        <Arrow direction = "left">
-            {/* Arror for left */}
+        <Arrow direction = "left" onClick={() => handleClick("left")}>
             <ArrowLeftOutlinedIcon/>
         </Arrow>
-        <Wrapper>
-            <Slide>
+        <Wrapper slideIndex = {slideIndex}>
+           {sliderItems.map((item) => (
+               
+            <Slide bg={item.bg}>
             <ImgContainer>
-            <Image src="../images/clothes4.jpg" alt="My Image" />
+            <Image src = {item.img} />
             </ImgContainer>
-            <InfoContainer></InfoContainer>
-            </Slide>
+            <InfoContainer>
+                <Title>{item.title} </Title>
+                <Description>{item.desc} </Description>
+                <Button>Shop Now</Button>
+            </InfoContainer>
+           </Slide>
+        ))}
         </Wrapper>
-        <Arrow direction = "right">
+        <Arrow direction = "right" onClick={() => handleClick("right")}>
             <ArrowRightOutlinedIcon/> 
         </Arrow>
     </Container>
---
+++ client/src/data.js
@@ -0,0 +1,27 @@
+import info1 from "./images/info1.jpg"
+import clothes1 from "./images/clothes1.jpg"
+import ribbon3 from "./images/ribbon3.jpg"
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: {info1},
+        title: "Stationery labels",
+        desc: "Don't hesitate, don't deliberate get your labels now!!",
+        bg: "#ca72da", 
+    },
+    {
+        id: 2,
+        img: {clothes1},
+        title: "Clothing labels",
+        desc: "Don't hesitate, don't deliberate get your labels now!!",
+        bg: "#8cdf58", 
+    },
+    {
+        id: 3,
+        img: {ribbon3},
+        title: "Ribbons",
+        desc: "Don't hesitate, don't deliberate get your labels now!!",
+        bg: "#ca72da", 
+    },
+]
